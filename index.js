3f5c7a6d0252fd2c4e3f26f77f1bd5bc32e0d255
qq6264214 <371998331@qq.com>
2019-05-15 15:42:46 +0800

    test
    新增normal模式下的自定义选择，wide模式选择范围

---
+++ index.js
@@ -0,0 +1,52 @@
+/**
+ * author:
+ * date: 2018-08-09 17:10:51
+ * desc:
+ */
+
+import Vue from 'vue';
+window.Vue = Vue;
+window.$ = window.jQuery = require('jquery');
+import App from './App';
+import {
+  Button,
+  Icon,
+  Switch,
+  Notification,
+  MessageBox,
+  Progress,
+  Slider,
+  Input,
+  Select,
+  Option
+} from 'element-ui';
+import './index.less';
+import 'element-ui/lib/theme-chalk/button.css';
+import 'element-ui/lib/theme-chalk/switch.css';
+import 'element-ui/lib/theme-chalk/notification.css';
+import 'element-ui/lib/theme-chalk/message-box.css';
+import 'element-ui/lib/theme-chalk/icon.css';
+import 'element-ui/lib/theme-chalk/progress.css';
+
+Vue.prototype.$ELEMENT = { size: 'medium' };
+Vue.prototype.$notify = Notification;
+
+Vue.use(Button);
+Vue.use(Switch);
+Vue.use(Icon);
+Vue.use(Progress);
+Vue.use(Slider)
+Vue.use(Input)
+Vue.use(Select)
+Vue.use(Option)
+
+let mode = location.href.indexOf('wide') > -1 ? 'wide' : 'normal';
+
+let vueapp = new Vue({
+  el: '#root',
+  data: { mode },
+  template: '<App/>',
+  components: { App }
+});
+
+window.app = vueapp.$children[0];
---
+++ rightNav.vue
@@ -0,0 +1,192 @@
+<template>
+  <div class="setting-nav">
+    <div class="burger-wrap">
+      <div :class="['nav-burger', showMenu ? 'active' : '']">
+        <div class="setting-icon" @click.prevent="openMenu">
+          <i v-if="showMenu" class="el-icon-d-arrow-right"></i>
+          <i v-else class="el-icon-setting"></i>
+        </div>
+      </div>
+    </div>
+    <transition name="black">
+      <div
+        class="black-screen"
+        v-show="showMenu"
+        @click.prevent="openMenu"
+      ></div>
+    </transition>
+    <transition name="side">
+      <div class="side-nav" v-show="showMenu">
+        <div class="side-header">
+          <h4>捉妖雷达 - Web</h4>
+          <p>{{ version }}</p>
+          <p>捉妖雷达开发者群：1025673494</p>
+          <p>虚拟定位全家暴毙</p>
+          <br/>
+          <iframe
+            src="https://ghbtns.com/github-btn.html?user=liuzirui1122&repo=zhuoyao_radar&type=star&count=true&size=large"
+            frameborder="0"
+            scrolling="0"
+            width="160px"
+            height="30px"
+          ></iframe>
+        </div>
+        <div class="side-content">
+          <div class="nav-filter">
+            <div class="header">筛选{{mode}}</div>
+            <ul>
+              <li>
+                <span class="tag">大范围模式</span>
+                <el-switch v-model="isWideMode"></el-switch>
+              </li>
+              <li v-if="mode === 'wide'">
+                <div></div>
+                <label class="tag">搜索范围: {{settings.wide_range}}</label>
+                <el-slider v-model="settings.wide_range" :max="10" :min="1" :step="1" style="float:right;width:50%;margin-top: -8px;margin-right: 2px;margin-right: 8px;"></el-slider>
+              </li>
+              
+            </ul>
+            <ul v-if="mode === 'normal'">
+              <template v-for="item in filters">
+                <li :key="item.key">
+                  <span class="tag">{{item.text}}</span>
+                  <el-switch v-model="settings.fit[item.key]"> </el-switch>
+                </li>
+              </template>
+            </ul>
+            <ul v-else>
+              <template v-for="item in settings.wide">
+                <li :key="item.id">
+                  <span class="tag">{{item.name}}</span>
+                  <el-switch v-model="item.on"> </el-switch>
+                </li>
+              </template>
+            </ul>
+          </div>
+          <div class="hr"></div>
+          <div class="nav-settings">
+            <div class="header">设置</div>
+            <ul>
+              <li>
+                <span class="tag">点击地图自动搜索</span>
+                <el-switch v-model="settings.auto_search"> </el-switch>
+              </li>
+              <li>
+                <span class="tag">显示剩余时间</span>
+                <el-switch v-model="settings.show_time"> </el-switch>
+              </li>
+              <li>
+                <span class="tag">记住上次退出位置</span>
+                <el-switch v-model="settings.position_sync"> </el-switch>
+              </li>
+              
+            </ul>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+<script>
+import { getLocalStorage } from '../lib/util';
+
+export default {
+  name: 'radar-right-nav',
+  props: {
+    version: {
+      type: String,
+      default: ''
+    },
+    settings: {
+      type: Object,
+      default: {}
+    },
+    showMenu: {
+      type: Boolean,
+      default: false
+    },
+    mode: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      filters: [
+        {
+          text: '稀有',
+          key: 'rare'
+        },
+        {
+          text: '1觉',
+          key: 't1'
+        },
+        {
+          text: '2觉',
+          key: 't2'
+        },
+        {
+          text: '巢穴',
+          key: 'nest'
+        },
+        {
+          text: '地域',
+          key: 'feature'
+        },
+        {
+          text: '鲲鲲',
+          key: 'fish'
+        },
+        {
+          text: '元素',
+          key: 'element'
+        },
+        {
+          text: '其他所有（慎选）',
+          key: 'all'
+        }
+      ],
+      isWideMode:false
+    };
+  },
+  methods: {
+    openMenu() {
+      this.$emit('update:showMenu', !this.showMenu);
+    },
+    changeMode(nVal){
+      debugger
+      let mode = ''
+      if(nVal){
+        mode='wide'
+      }else{
+        mode='normal'
+      }
+      this.$emit('modeChanged',mode)
+    }
+    
+  },
+  watch: {
+    isWideMode(nVal){
+      this.changeMode(nVal)
+    }
+  },
+};
+</script>
+<style lang="less">
+.setting-nav {
+  font-size: 14px;
+}
+.side-content {
+  .header {
+    font-size: 16px;
+    padding-left: 10px;
+    padding-top: 5px;
+  }
+}
+// .el-tabs__item {
+//   padding: 0 20px !important;
+// }
+// .el-tabs__active-bar{
+//   display: none !important;
+// }
+</style>
